993da6e979a564665e02fe08f4256110e7634c33
Transwert <37514124+Transwert@users.noreply.github.com>
2019-08-22 21:31:08 +0530

    Update tweepy_streamer.py

--- tweepy_streamer.py
+++ tweepy_streamer.py
@@ -1,60 +1,57 @@
-from tweepy.streaming import StreamListener 
-# class from tweepy module responsible for to listen tweets,
-# on the basis of certain Hashtags,
+# YouTube Video: https://www.youtube.com/watch?v=wlnx-7cm4Gg
+from tweepy.streaming import StreamListener
 from tweepy import OAuthHandler
-# class responsible for authenticating
-# credentials which we stored in other files.
 from tweepy import Stream
-
+ 
 import twitter_credentials
-# importing credential file
-
+ 
+# # # # TWITTER STREAMER # # # #
 class TwitterStreamer():
-	'''
-	Class for streaming and processing live tweets.
-	'''
-	def __init(self):
-		pass
+    """
+    Class for streaming and processing live tweets.
+    """
+    def __init__(self):
+        pass
 
-	def stream_tweets(self,fetched_tweets_filename, hash_tag_list):
-		# this handles Twitter authentication and connection to the Twitter Streaming API.
+    def stream_tweets(self, fetched_tweets_filename, hash_tag_list):
+        # This handles Twitter authetification and the connection to Twitter Streaming API
+        listener = StdOutListener(fetched_tweets_filename)
+        auth = OAuthHandler(twitter_credentials.CONSUMER_KEY, twitter_credentials.CONSUMER_SECRET)
+        auth.set_access_token(twitter_credentials.ACCESS_TOKEN, twitter_credentials.ACCESS_TOKEN_SECRET)
+        stream = Stream(auth, listener)
 
-		listener = StdOutListener()
-		auth = OAuthHandler(twitter_credentials.CONSUMER_KEY, twitter_credentials.CONSUMER_SECRET)
-		auth.set_access_token(twitter_credentials.ACCESS_TOKEN, twitter_credentials.ACCESS_TOKEN_SECRET)
+        # This line filter Twitter Streams to capture data by the keywords: 
+        stream.filter(track=hash_tag_list)
 
-		stream = Stream(auth,listener)
 
-		stream.filter(track = hash_tag_list)
+# # # # TWITTER STREAM LISTENER # # # #
+class StdOutListener(StreamListener):
+    """
+    This is a basic listener that just prints received tweets to stdout.
+    """
+    def __init__(self, fetched_tweets_filename):
+        self.fetched_tweets_filename = fetched_tweets_filename
 
-class StdOutListener( StreamListener ):
-	'''
-	Class to print received tweets to StdOUT,
-	which inherit from StreamListener class,
-	( which provides many in built methods. )
-	'''
-	def __init__(self, fetched_tweets_filename):
-		self.fetched_tweets_filename = fetched_tweets_filename
+    def on_data(self, data):
+        try:
+            print(data)
+            with open(self.fetched_tweets_filename, 'a') as tf:
+                tf.write(data)
+            return True
+        except BaseException as e:
+            print("Error on_data %s" % str(e))
+        return True
+          
 
-	def on_data(self, data):
-		# used to read data from Streamlistner
-		try:
-			print(data)
-			with open(self.fetched_tweets_filename,'a') as tf:
-				tf.write(data)
-			return True
-		except BaseException as e:
-			print("Error on_data: %s" % str(e))
-		return True
+    def on_error(self, status):
+        print(status)
 
-	def on_error(self, status):
-		# if error occurs
-		print(status)
+ 
+if __name__ == '__main__':
+ 
+    # Authenticate using config.py and connect to Twitter Streaming API.
+    hash_tag_list = ["donal trump", "hillary clinton", "barack obama", "bernie sanders"]
+    fetched_tweets_filename = "tweets.txt"
 
-if __name__ == "__main__":
-	
-	hash_tag_list = ["kubernetes","open","source"]
-	fetched_tweets_filename = "tweets.json"
-	
-	twitter_streamer = TwitterStreamer()
-	twitter_streamer.stream_tweets(fetched_tweets_filename , hash_tag_list)
+    twitter_streamer = TwitterStreamer()
+    twitter_streamer.stream_tweets(fetched_tweets_filename, hash_tag_list)
