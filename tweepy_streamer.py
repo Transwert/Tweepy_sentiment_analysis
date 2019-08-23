b731a25fb6bb93c45c662e607411ac5da65aec1b
Transwert <37514124+Transwert@users.noreply.github.com>
2019-08-23 19:40:58 +0530

    Update tweepy_streamer.py

--- tweepy_streamer.py
+++ tweepy_streamer.py
@@ -1,13 +1,19 @@
-from tweepy.streaming import StreamListener 
-# class from tweepy module responsible for to listen tweets,
-# on the basis of certain Hashtags,
+from tweepy.streaming import StreamListener
+'''
+Class from tweepy module responsible for to listen tweets,
+on the basis of certain Hashtags,
+'''
+
 from tweepy import OAuthHandler
-# class responsible for authenticating
-# credentials which we stored in other files.
+'''
+Class responsible for authenticating
+credentials which we stored in other files.
+'''
+
 from tweepy import Stream
 
 import twitter_credentials
-# importing credential file
+# Importing credential file
 
 class TwitterStreamer():
 	'''
@@ -17,16 +23,26 @@ class TwitterStreamer():
 		pass
 
 	def stream_tweets(self,fetched_tweets_filename, hash_tag_list):
-		# this handles Twitter authentication and connection to the Twitter Streaming API.
-
+		'''
+		This handles Twitter authentication and connection to the Twitter Streaming API,
+		along with printing the tweets in compiler as well as 
+		saving them in dump file with stated format
+		'''
 		listener = StdOutListener(fetched_tweets_filename)
 		auth = OAuthHandler(twitter_credentials.CONSUMER_KEY, twitter_credentials.CONSUMER_SECRET)
 		auth.set_access_token(twitter_credentials.ACCESS_TOKEN, twitter_credentials.ACCESS_TOKEN_SECRET)
 
 		stream = Stream(auth,listener)
+		'''
+		Main twitter stream, which has authentication token and 
+		method of how the data to be handle.
+		'''
 
 		stream.filter(track = hash_tag_list)
-
+		'''
+		Inbuilt method to filter out the required tweets from all received tweets
+		on the basis of parameters in form of list, keywords, etc.
+		'''
 class StdOutListener( StreamListener ):
 	'''
 	Class to print received tweets to StdOUT,
@@ -40,21 +56,31 @@ class StdOutListener( StreamListener ):
 		# used to read data from Streamlistner
 		try:
 			print(data)
+
 			with open(self.fetched_tweets_filename,'a') as tf:
+				'''
+				Utility to write tweets [ which are in form of JSON format dict. object ]
+				containing all the information, about the tweet, origin of tweet
+				who retweeted, etc.
+				'''
 				tf.write(data)
 			return True
+			
 		except BaseException as e:
 			print("Error on_data: %s" % str(e))
 		return True
 
 	def on_error(self, status):
-		# if error occurs
+		# if error occurs, prints it out
 		print(status)
 
 if __name__ == "__main__":
 	
 	hash_tag_list = ["kubernetes","open","source"]
-	fetched_tweets_filename = "tweets.json"
+	# list of the hashtags which will be used to filter out needed tweets
+
+	fetched_tweets_filename = "garbage_tweets.json"
+	# file in which filtered tweets will be saved
 	
 	twitter_streamer = TwitterStreamer()
 	twitter_streamer.stream_tweets(fetched_tweets_filename , hash_tag_list)
